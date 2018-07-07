3137e34eff35e6344acde51eccde6b7f2517346f
bburnett86 <breton.burnett@gmail.com>
2018-07-07 11:47:14 -0500

    Create backend validations and add frontend email tag

--- app/models/selection.rb
+++ app/models/selection.rb
@@ -1,5 +1,8 @@
 class Selection < ApplicationRecord
 
+  validates :genre_id, :user_id, presence: true
+  validates :genre_id, :user_id, numericality: { only_integer: true }
+
   belongs_to :genre
   belongs_to :user
 end
